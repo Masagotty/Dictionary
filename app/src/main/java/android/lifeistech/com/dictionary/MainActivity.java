3d2d575b60758025c19df48be157625fe5e77cd7
KentaMasago <masagotty@gmail.com>
2016-06-10 21:45:41 +0900

    check

--- app/src/main/java/android/lifeistech/com/dictionary/MainActivity.java
+++ app/src/main/java/android/lifeistech/com/dictionary/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
@@ -58,6 +59,28 @@ public class MainActivity extends AppCompatActivity {
         }
 
         listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                String item = (String)adapter.getItem(position);
+                Toast.makeText(getApplicationContext(), item + "を削除しました", Toast.LENGTH_SHORT).show();
+                adapter.remove(item);
+
+            }
+        });
+
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+
+                Toast.makeText(getApplicationContext(), "すべて削除しました", Toast.LENGTH_SHORT).show();
+                adapter.clear();
+
+                return false;
+            }
+        });
 
     }
 
